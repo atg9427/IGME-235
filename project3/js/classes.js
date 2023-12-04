a0fadb6ee55d3963a67a77799860f88cf5289a0b
Alexander Gough <atg9427@rit.edu>
2023-12-04 11:21:52 -0500

    Commit

--- project3/js/classes.js
+++ project3/js/classes.js
@@ -38,6 +38,66 @@ class Virus extends PIXI.Sprite {
     }
 }
 
+class Bacteria extends PIXI.Sprite {
+    constructor(radius, color = 0xFF0000, x = 0, y = 0) {
+        super(gameWindow.loader.resources["game-images/escherichia.png"].texture);
+        this.beginFill(color);
+        this.drawCircle(0, 0, radius);
+        this.endFill();
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+
+        // variables
+        this.fwd = getRandomUnitVector();
+        this.speed = 50;
+        this.isAlive = true;
+    }
+
+    move(dt = 1 / 60) {
+        this.x += this.fwd.x * this.speed * dt;
+        this.y += this.fwd.y * this.speed * dt;
+    }
+
+    reflectX() {
+        this.fwd.x *= -1;
+    }
+
+    reflectY() {
+        this.fwd.y *= -1;
+    }
+}
+
+class Helminth extends PIXI.Sprite {
+    constructor(radius, color = 0xFF0000, x = 0, y = 0) {
+        super(gameWindow.loader.resources["game-images/helminth_spritesheet.png"].texture);
+        this.beginFill(color);
+        this.drawCircle(0, 0, radius);
+        this.endFill();
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+
+        // variables
+        this.fwd = getRandomUnitVector();
+        this.speed = 50;
+        this.isAlive = true;
+    }
+
+    move(dt = 1 / 60) {
+        this.x += this.fwd.x * this.speed * dt;
+        this.y += this.fwd.y * this.speed * dt;
+    }
+
+    reflectX() {
+        this.fwd.x *= -1;
+    }
+
+    reflectY() {
+        this.fwd.y *= -1;
+    }
+}
+
 class Projectile extends PIXI.Graphics {
     constructor(color = 0xFFFFFF, x = 0, y = 0) {
         super();
